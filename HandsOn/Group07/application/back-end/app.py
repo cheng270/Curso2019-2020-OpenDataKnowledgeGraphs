a5ce6bb830cb8b8072652a8e648ff06f6c6f565b
GiacomoZiffer <10532347@polimi.it>
2019-10-24 21:35:16 +0200

    start of rdflib python

--- HandsOn/Group07/application/back-end/app.py
+++ HandsOn/Group07/application/back-end/app.py
@@ -1,9 +1,29 @@
 from flask import Flask
 from flask import render_template
 from flask import make_response
+from rdflib import Graph
 
 app = Flask(__name__, template_folder="../front-end/templates", static_folder="../front-end/static")
+foaf = Graph()
+foaf.parse("../../rdf/rdf-with-links.ttl", format="turtle")
+print(list(foaf.query("""SELECT DISTINCT ?Concept
+                     WHERE {
+                     ?x a ?Concept .
+                     } LIMIT 1
+                     """))[0].Concept)
 
+def query():
+    res = foaf.query("""select distinct ?x 
+                        WHERE 
+                        {
+                          ?x a <http://www.airqualitymadrid.es/ontologies/airqualitymadrid#Pollutant> .
+                        } LIMIT 100
+                        """)
+    tmp = list()
+    for row in res:
+        d = str(row['x'].toPython())
+        tmp.append(d)
+    return tmp
 
 @app.route('/')
 def show_home_page():
@@ -12,7 +32,9 @@ def show_home_page():
 
 @app.route("/<test>")
 def show_test_page(test):
-    return render_template(test + ".html")
+    data = query()
+    print(data)
+    return render_template(test + ".html", result=data)
 
 
 @app.errorhandler(404)
