1f337a09ffdcf473ae3fb453c3b77530b12c751f
romeopaolo <paolo1.romeo@mail.polimi.it>
2019-10-31 00:24:35 +0100

    fixed error in endopoints

--- HandsOn/Group07/application/back-end/app.py
+++ HandsOn/Group07/application/back-end/app.py
@@ -17,24 +17,23 @@ def send_stations():
     return retriever.get_stations()
 
 
-@app.route('/station_id/<station_id>/day/<date>')
-def send_measurements_by_id_and_date(station_id, date):
-    return retriever.get_measurements_by_id_and_date(station_id, date)
+@app.route('/station_id/<station_id>/start_date/<start_date>/end_date/<end_date>')
+def send_measurements_by_id_and_date(station_id, start_date, end_date):
+    return retriever.get_measurements_by_id_and_date(station_id, start_date, end_date)
 
 
 @app.route("/pollutant/<pollutant_id>")
 def show_test_page(pollutant_id):
-    data = retriever.get_pollutant_data(pollutant_id)
-    return render_template("test.html", result=data)
+    return retriever.get_pollutant_data(pollutant_id)
 
 
+# todo: check how to display errors
 @app.errorhandler(404)
 def not_found():
     """404 template."""
     return make_response(render_template("404.html"), 404)
 
 
-# todo: check how to display errors
 @app.errorhandler(500)
 def not_found():
     """500 template."""
